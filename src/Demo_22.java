83c9febacfa797312af07b64a82020d85356cb81
wenzhan <1694430501@qq.om>
2022-03-31 10:33:23 +0800

    return跟finally返回

--- src/Demo_22.java
+++ src/Demo_22.java
@@ -1,9 +1,17 @@
 /**
  * @author: wz
- * @date: 2022/03/16
+ * @date: 2022/03/31
+ * return跟finally返回
  */
 public class Demo_22 {
     public static void main(String[] args) {
-
+        System.out.println(print());
+    }
+    public static int print(){
+        try{
+            return 2;
+        }finally {
+            return 3;
+        }
     }
 }
